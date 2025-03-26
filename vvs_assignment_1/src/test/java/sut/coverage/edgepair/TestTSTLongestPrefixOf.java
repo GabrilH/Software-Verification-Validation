a82337b25e4e3cc0172ae2ac035faf5367f579be
Gabriel Henriques <fc58182@alunos.fc.ul.pt>
2025-03-26 11:11:49 +0000

    redone prime path and edgepair coverage

--- vvs_assignment_1/src/test/java/sut/coverage/edgepair/TestTSTLongestPrefixOf.java
+++ vvs_assignment_1/src/test/java/sut/coverage/edgepair/TestTSTLongestPrefixOf.java
@@ -1,7 +1,5 @@
 package sut.coverage.edgepair;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -30,51 +28,65 @@ public class TestTSTLongestPrefixOf {
 
     @Test 
     public void testWithoutPuts() {
-        // [1,3,5] [3,5,6] [5,6,16]
+        // [1,3,5,6,16]
+        //
+        // [1,3,5], [3,5,6], [5,6,16]
         TST<Integer> tst = new TST<>();
         assert(tst.longestPrefixOf("a").equals(""));
     }
 
     @Test
-    public void testWithOnePutQueryHeadAtLeft() {
-        // [5,6,7] [6,7,8] [7,8,9] [8,9,6] [9,6,16]
-        TST<Integer> tst = new TST<>();
-        tst.put("b", 1);
-        assert(tst.longestPrefixOf("a").equals(""));
-    }
-
-    @Test
-    public void testWithOnePutQueryHeadAtRight() {
-        // [7,8,10] [8,10,11] [10,11,6] [11,6,16]
+    public void testMiddleMatchThenLeftThenExit() {
+        // [1,3,5,6,7,8,10,12,13,14,15,6,7,8,9,6,16]
+        // MiddleMatchThenLeftThenExit
+        //
+        // [1,3,5], [3,5,6], [5,6,7], [6,7,8], [7,8,9], 
+        // [7,8,10], [8,9,6], [8,10,12], [9,6,16], [10,12,13], 
+        // [12,13,14], [13,14,15], [14,15,6], [15,6,7]
         TST<Integer> tst = new TST<>();
         tst.put("a", 1);
+        tst.put("ab", 2);
+        assert(tst.longestPrefixOf("aa").equals("a"));
+    }
+
+    @Test
+    public void testRightThenLeftThenExit() {
+        // [1,3,5,6,7,8,10,11,6,7,8,9,6,16]
+        // RightThenLeftThenExit
+        // 
+        // [1,3,5], [3,5,6], [5,6,7], [6,7,8], [7,8,9], 
+        // [7,8,10], [8,9,6], [8,10,11], [9,6,16], 
+        // [10,11,6], [11,6,7]
+        TST<Integer> tst = new TST<>();
+        tst.put("c", 1);
+        tst.put("e", 2);
+        assert(tst.longestPrefixOf("d").equals(""));
+    }
+
+    @Test
+    public void testLeftThenRightThenExit() {
+        // [1,3,5,6,7,8,9,6,7,8,10,11,6,16]
+        // LeftThenRightThenExit
+        // 
+        // [1,3,5], [3,5,6], [5,6,7], [6,7,8], 
+        // [7,8,9], [7,8,10], [8,9,6], [8,10,11], 
+        // [9,6,7], [10,11,6], [11,6,16]
+        TST<Integer> tst = new TST<>();
+        tst.put("c", 1);
+        tst.put("a", 2);
         assert(tst.longestPrefixOf("b").equals(""));
     }
 
     @Test
-    public void testWithOnePutQueryEqual() {
-        // [8,10,12] [10,12,13] [12,13,14] [13,14,15] [14,15,6]
-        // [15,6,16]
+    public void testMiddleNoMatchThenExit() {
+        // [1,3,5,6,7,8,10,12,13,15,6,16]
+        // MiddleNoMatchThenExit
+        //
+        // [1,3,5], [3,5,6], [5,6,7], [6,7,8], 
+        // [7,8,10], [8,10,12], [10,12,13], 
+        // [12,13,15], [13,15,6], [15,6,16]
         TST<Integer> tst = new TST<>();
-        tst.put("a", 1);
-        assert(tst.longestPrefixOf("a") == "a");
-    }
-
-    @Test 
-    public void testWithOnePutQuerySubstring() {
-        // [12,13,15] [13,15,6] [15,6,7]
-        TST<Integer> tst = new TST<>();
-        tst.put("app", 1);
-        assert(tst.longestPrefixOf("apple").equals("app"));
-    }
-
-    @Test
-    public void testThreePutsQueryBothRightAndLeft() {
-        // [9,6,7] [11,6,7]
-        TST<Integer> tst = new TST<>();
-        tst.put("caju", 1);
-        tst.put("ananas", 2);
-        tst.put("ban", 3);
-        assert(tst.longestPrefixOf("banana").equals("ban"));
+        tst.put("casa", 1);
+        assert(tst.longestPrefixOf("c").equals(""));
     }
 }
--- vvs_assignment_1/src/test/java/sut/coverage/primepath/TestTSTLongestPrefixOf.java
+++ vvs_assignment_1/src/test/java/sut/coverage/primepath/TestTSTLongestPrefixOf.java
@@ -34,126 +34,235 @@ public class TestTSTLongestPrefixOf {
     }
 
     @Test
-    public void testOneToTheLeftExit() {
-        // [7,8,9,6,16] [6,7,8,9,6]
+    public void testLeftThenLeftThenExit() {
+        // [1,3,5,6,7,8,9,6,7,8,9,6,16]:
+        // LeftThenLeftThenExit
+        //
+        // [1,3,5,6,7,8,9], [7,8,9,6,7], 
+        // [7,8,9,6,16], [8,9,6,7,8], 
+        // [6,7,8,9,6], [9,6,7,8,9]
         TST<Integer> tst = new TST<>();
-        tst.put("danone", 1);
-        assert(tst.longestPrefixOf("ananas").equals(""));
-    }
-
-    @Test
-    public void testOneToTheRightExit() {
-        // [7,8,10,11,6,16] [6,7,8,10,11,6]
-        TST<Integer> tst = new TST<>();
-        tst.put("danone", 1);
-        assert(tst.longestPrefixOf("fotografia").equals(""));
-    }
-
-    @Test
-    public void testQueryHeadInMiddleNoMatch() {
-        // [1,3,5,6,7,8,10,12,13,15] [7,8,10,12,13,15,6,16]
-        TST<Integer> tst = new TST<>();
-        tst.put("aa", 1);
+        tst.put("c", 1);
+        tst.put("b", 2);
         assert(tst.longestPrefixOf("a").equals(""));
     }
 
     @Test
-    public void testQueryHeadInMiddleMatch() {
-        // [1,3,5,6,7,8,10,12,13,14,15] [7,8,10,12,13,14,15,6,16]
+    public void testRightThenRightThenExit() {
+        // [1,3,5,6,7,8,10,11,6,7,8,10,11,6,16]	
+        // RightThenRightThenExit
+        // 
+        // [1,3,5,6,7,8,10,11], [8,10,11,6,7,8], 
+        // [7,8,10,11,6,7], [7,8,10,11,6,16], 
+        // [6,7,8,10,11,6], [10,11,6,7,8,10], 
+        // [11,6,7,8,10,11]
         TST<Integer> tst = new TST<>();
-        tst.put("a", 1);
-        assert(tst.longestPrefixOf("ana").equals("a"));
+        tst.put("c", 1);
+        tst.put("d", 2);
+        assert(tst.longestPrefixOf("e").equals(""));
     }
 
     @Test
-    public void testFirstTwoToTheLeft() {
-        // [9,6,7,8,9] [8,9,6,7,8] [7,8,9,6,7] [1,3,5,6,7,8,9]
+    public void testLeftThenRightThenExit() {
+        // [1,3,5,6,7,8,9,6,7,8,10,11,6,16]	
+        // LeftThenRightThenExit
+        //
+        // [1,3,5,6,7,8,9], [9,6,7,8,10,11], 
+        // [7,8,10,11,6,16], [6,7,8,10,11,6], 
+        // [7,8,9,6,7], [8,9,6,7,8], [6,7,8,9,6]
         TST<Integer> tst = new TST<>();
-        tst.put("danone", 1);
-        tst.put("caju", 2);
-        tst.put("ban", 3);
-        assert(tst.longestPrefixOf("banana").equals("ban"));
-    }
-    
-    @Test
-    public void testFirstTwoToTheRight() {
-        // [11,6,7,8,10,11] [7,8,10,11,6,7] [8,10,11,6,7,8] [10,11,6,7,8,10] [1,3,5,6,7,8,10,11]
-        TST<Integer> tst = new TST<>();
-        tst.put("danone", 1);
-        tst.put("eagan", 2);
-        tst.put("foto", 3);
-        assert(tst.longestPrefixOf("fotografia").equals("foto"));
+        tst.put("c", 1);
+        tst.put("a", 2);
+        assert(tst.longestPrefixOf("b").equals(""));
     }
 
     @Test
-    public void testRightThenLeftThenMiddle() {
-        // [10,11,6,7,8,9] [9,6,7,8,10,12,13,15]
+    public void testRightThenLeftThenExit() {
+        // [1,3,5,6,7,8,10,11,6,7,8,9,6,16]	
+        // RightThenLeftThenExit
+        //
+        // [1,3,5,6,7,8,10,11], [8,10,11,6,7,8], 
+        // [7,8,10,11,6,7], [6,7,8,10,11,6], 
+        // [10,11,6,7,8,9], [7,8,9,6,16], [6,7,8,9,6]
         TST<Integer> tst = new TST<>();
-        tst.put("danone", 1);
-        tst.put("foto", 2);
-        tst.put("eag", 3);
-        assert(tst.longestPrefixOf("eagan").equals("eag"));
+        tst.put("c", 1);
+        tst.put("e", 2);
+        assert(tst.longestPrefixOf("d").equals(""));
     }
 
     @Test
-    public void testLeftThenRightThenMiddle() {
-        // [9,6,7,8,10,11] [11,6,7,8,10,12,13,15]
+    public void testRightThenMiddleNoMatchThenExit() {
+        // [1,3,5,6,7,8,10,11,6,7,8,10,12,13,15,6,16]	
+        // RightThenMiddleNoMatchThenExit
+        //
+        // [7,8,10,12,13,15,6,16], [1,3,5,6,7,8,10,11], 
+        // [6,7,8,10,12,13,15,6], [11,6,7,8,10,12,13,15], 
+        //[8,10,11,6,7,8], [7,8,10,11,6,7], [6,7,8,10,11,6], 
+        // [10,11,6,7,8,10]
         TST<Integer> tst = new TST<>();
-        tst.put("danone", 1);
-        tst.put("ananas", 2);
-        tst.put("ban", 3);
-        assert(tst.longestPrefixOf("banana").equals("ban"));
+        tst.put("c", 1);
+        tst.put("da", 2);
+        assert(tst.longestPrefixOf("d").equals(""));	
     }
 
     @Test
-    public void testMiddleNoMatchThenMiddleNoMatch() {
-        // [6,7,8,10,12,13,15,6] [12,13,15,6,7,8,10,12] 
-        // [13,15,6,7,8,10,12,13] [15,6,7,8,10,12,13,15]
-        // [10,12,13,15,6,7,8,10] [8,10,12,13,15,6,7,8]
-        // [7,8,10,12,13,15,6,7]
+    public void testMiddleNoMatchThenRightThenExit() {
+        // [1,3,5,6,7,8,10,12,13,15,6,7,8,10,11,6,16]	
+        // MiddleNoMatchThenRightThenExit
+        //
+        // [1,3,5,6,7,8,10,12,13,15], [7,8,10,12,13,15,6,7], 
+        // [8,10,12,13,15,6,7,8], [6,7,8,10,12,13,15,6], 
+        // [12,13,15,6,7,8,10,11], [10,12,13,15,6,7,8,10], 
+        // [7,8,10,11,6,16], [6,7,8,10,11,6]
         TST<Integer> tst = new TST<>();
-        tst.put("ana", 1);
-        assert(tst.longestPrefixOf("ananas").equals("ana"));
+        tst.put("ca", 1);
+        assert(tst.longestPrefixOf("cb").equals(""));
     }
 
     @Test
-    public void testMiddleMatchThenMiddleMatch() {
-        // [6,7,8,10,12,13,14,15,6] [7,8,10,12,13,14,15,6,7]
-        // [8,10,12,13,14,15,6,7,8] [13,14,15,6,7,8,10,12,13]
-        // [12,13,14,15,6,7,8,10,12] [14,15,6,7,8,10,12,13,14]
-        // [15,6,7,8,10,12,13,14,15] [10,12,13,14,15,6,7,8,10]
+    public void testMiddleNoMatchThenMiddleNoMatchThenExit() {
+        // [1,3,5,6,7,8,10,12,13,15,6,7,8,10,12,13,15,6,16]
+        // MiddleNoMatchThenMiddleNoMatchThenExit
+        //
+        // [1,3,5,6,7,8,10,12,13,15], [7,8,10,12,13,15,6,7], 
+        // [7,8,10,12,13,15,6,16], [8,10,12,13,15,6,7,8], 
+        // [6,7,8,10,12,13,15,6], [12,13,15,6,7,8,10,12], 
+        // [13,15,6,7,8,10,12,13], [15,6,7,8,10,12,13,15], 
+        // [10,12,13,15,6,7,8,10]
+        TST<Integer> tst = new TST<>();
+        tst.put("caa", 1);
+        assert(tst.longestPrefixOf("ca").equals(""));
+    }
+
+    @Test
+    public void testLeftThenMiddleNoMatchThenExit() {
+        // [1,3,5,6,7,8,9,6,7,8,10,12,13,15,6,16]	
+        // LeftThenMiddleNoMatchThenExit
+        //
+        // [7,8,10,12,13,15,6,16], [9,6,7,8,10,12,13,15], 
+        // [6,7,8,10,12,13,15,6], [1,3,5,6,7,8,9], [7,8,9,6,7], 
+        // [8,9,6,7,8], [6,7,8,9,6]
+        TST<Integer> tst = new TST<>();
+        tst.put("c", 1);
+        tst.put("ba", 2);
+        assert(tst.longestPrefixOf("b").equals(""));
+    }
+
+    @Test
+    public void testMiddleNoMatchThenLeftThenExit() {
+        // [1,3,5,6,7,8,10,12,13,15,6,7,8,9,6,16]
+        // MiddleNoMatchThenLeftThenExit
+        // 
+        // [1,3,5,6,7,8,10,12,13,15], [7,8,10,12,13,15,6,7], 
+        // [8,10,12,13,15,6,7,8], [6,7,8,10,12,13,15,6], 
+        // [10,12,13,15,6,7,8,9], [7,8,9,6,16], [6,7,8,9,6]
+        TST<Integer> tst = new TST<>();
+        tst.put("cb", 1);
+        assert(tst.longestPrefixOf("ca").equals(""));
+    }
+
+    @Test
+    public void testRigthThenMiddleMatchThenExit() {
+        // [1,3,5,6,7,8,10,11,6,7,8,10,12,13,14,15,6,16]
+        // RigthThenMiddleMatchThenExit
+        //
+        // [7,8,10,12,13,14,15,6,16], [6,7,8,10,12,13,14,15,6], 
+        // [11,6,7,8,10,12,13,14,15], [1,3,5,6,7,8,10,11], 
+        // [8,10,11,6,7,8], [7,8,10,11,6,7], [6,7,8,10,11,6], 
+        // [10,11,6,7,8,10]
+        TST<Integer> tst = new TST<>();
+        tst.put("c", 1);
+        tst.put("d", 2);
+        assert(tst.longestPrefixOf("d").equals("d"));
+    }
+
+    @Test
+    public void testMiddleMatchThenRightThenExit() {
+        // [1,3,5,6,7,8,10,12,13,14,15,6,7,8,10,11,6,16]	
+        // MiddleMatchThenRightThenExit
+        //
+        // [1,3,5,6,7,8,10,12,13,14,15], [6,7,8,10,12,13,14,15,6], 
+        // [7,8,10,12,13,14,15,6,7], [8,10,12,13,14,15,6,7,8], 
+        // [12,13,14,15,6,7,8,10,11], [10,12,13,14,15,6,7,8,10], 
+        // [7,8,10,11,6,16], [6,7,8,10,11,6]
+        TST<Integer> tst = new TST<>();
+        tst.put("c", 1);
+        tst.put("ca", 2);
+        assert(tst.longestPrefixOf("cd").equals("c"));
+    }
+
+    @Test
+    public void testMiddleNoMatchThenMiddleMatchThenExit() {
+        // [1,3,5,6,7,8,10,12,13,15,6,7,8,10,12,13,14,15,6,16]	
+        // MiddleNoMatchThenMiddleMatchThenExit
+        //
+        // [1,3,5,6,7,8,10,12,13,15], [7,8,10,12,13,14,15,6,16], 
+        // [6,7,8,10,12,13,14,15,6], [15,6,7,8,10,12,13,14,15], 
+        // [7,8,10,12,13,15,6,7], [8,10,12,13,15,6,7,8], 
+        // [6,7,8,10,12,13,15,6], [12,13,15,6,7,8,10,12], 
+        // [13,15,6,7,8,10,12,13], [10,12,13,15,6,7,8,10]
+        TST<Integer> tst = new TST<>();
+        tst.put("ca", 1);
+        assert(tst.longestPrefixOf("ca").equals("ca"));
+    }
+
+    @Test
+    public void testMiddleMatchThenMiddleMatchThenExit() {
+        // [1,3,5,6,7,8,10,12,13,14,15,6,7,8,10,12,13,14,15,6,16]
+        // MiddleMatchThenMiddleMatchThenExit
+        //
+        // [1,3,5,6,7,8,10,12,13,14,15], [7,8,10,12,13,14,15,6,16], 
+        // [6,7,8,10,12,13,14,15,6], [7,8,10,12,13,14,15,6,7], 
+        // [8,10,12,13,14,15,6,7,8], [13,14,15,6,7,8,10,12,13], 
+        // [12,13,14,15,6,7,8,10,12], [14,15,6,7,8,10,12,13,14],
+        // [15,6,7,8,10,12,13,14,15], [10,12,13,14,15,6,7,8,10]
+        TST<Integer> tst = new TST<>();
+        tst.put("c", 1);
+        tst.put("ca", 2);
+        assert(tst.longestPrefixOf("ca").equals("ca"));	
+    }
+
+    @Test
+    public void testMiddleMatchThenMiddleNoMatchThenExit() {
+        // [1,3,5,6,7,8,10,12,13,14,15,6,7,8,10,12,13,15,6,16]	
+        // MiddleMatchThenMiddleNoMatchThenExit
+        //
+        // [1,3,5,6,7,8,10,12,13,14,15], [6,7,8,10,12,13,14,15,6], 
+        // [7,8,10,12,13,14,15,6,7], [8,10,12,13,14,15,6,7,8], 
+        // [13,14,15,6,7,8,10,12,13], [12,13,14,15,6,7,8,10,12],
+        // [10,12,13,14,15,6,7,8,10], [7,8,10,12,13,15,6,16], 
+        // [6,7,8,10,12,13,15,6], [15,6,7,8,10,12,13,15]
+        TST<Integer> tst = new TST<>();
+        tst.put("c", 1);
+        tst.put("cab", 2);
+        assert(tst.longestPrefixOf("ca").equals("c"));
+    }
+
+    @Test
+    public void testLeftThenMiddleMatchThenExit() {
+        // [1,3,5,6,7,8,9,6,7,8,10,12,13,14,15,6,16]
+        // LeftThenMiddleMatchThenExit
+        // 
+        // [7,8,10,12,13,14,15,6,16], [6,7,8,10,12,13,14,15,6], 
+        // [9,6,7,8,10,12,13,14,15], [1,3,5,6,7,8,9], [7,8,9,6,7], 
+        // [8,9,6,7,8], [6,7,8,9,6]
+        TST<Integer> tst = new TST<>();
+        tst.put("c", 1);
+        tst.put("a", 2);
+        assert(tst.longestPrefixOf("a").equals("a"));
+    }
+
+    @Test
+    public void testMiddleMatchThenLeftThenExit() {
+        // [1,3,5,6,7,8,10,12,13,14,15,6,7,8,9,6,16]
+        // MiddleMatchThenLeftThenExit
+        // 	
+        // [1,3,5,6,7,8,10,12,13,14,15], [6,7,8,10,12,13,14,15,6], 
+        // [7,8,10,12,13,14,15,6,7], [8,10,12,13,14,15,6,7,8], 
+        // [10,12,13,14,15,6,7,8,9], [7,8,9,6,16], [6,7,8,9,6]
         TST<Integer> tst = new TST<>();
         tst.put("a", 1);
         tst.put("ab", 2);
-        assert(tst.longestPrefixOf("abana").equals("ab"));
-    }
-
-    @Test
-    public void testRightThenMiddleMatchThenRight() {
-        // [11,6,7,8,10,12,13,14,15] [12,13,14,15,6,7,8,10,11]
-    }
-
-    @Test
-    public void testMiddleNoMatchThenRightThenMiddleMatchThenRight() {
-        // [12,13,15,6,7,8,10,11]
-        // [11,6,7,8,10,12,13,14,15]
-        // [12,13,14,15,6,7,8,10,11]
-        TST<Integer> tst = new TST<>();
-        tst.put("danone", 1);
-        tst.put("de",2);
-        tst.put("dentes",2);
-        assert(tst.longestPrefixOf("depois").equals("de"));
-    }
-
-    @Test
-    public void testMiddleNoMatchThenLeftThenMiddleMatchThenLeft() {
-        // [10,12,13,15,6,7,8,9]
-        // [9,6,7,8,10,12,13,14,15]
-        // [10,12,13,14,15,6,7,8,9]
-        TST<Integer> tst = new TST<>();
-        tst.put("desejo", 1);
-        tst.put("danone", 2);
-        tst.put("da", 3);
-        assert(tst.longestPrefixOf("dados").equals("da"));
+        assert(tst.longestPrefixOf("aa").equals("a"));
     }
 }
