e304c7ff75549a7299854e5ea883b3d9c014fc5f
Gabriel Henriques <fc58182@alunos.fc.ul.pt>
2025-03-27 10:51:32 +0000

    changed line tests to cover just enough

--- vvs_assignment_1/src/test/java/sut/coverage/line/TestTSTKeysThatMatch.java
+++ vvs_assignment_1/src/test/java/sut/coverage/line/TestTSTKeysThatMatch.java
@@ -10,17 +10,10 @@ import sut.TST;
 public class TestTSTKeysThatMatch {
     
     @Test
-    public void testNormalCase() {
+    public void testEmptyKeys() {
+        // linhas 210, 211, 212
         TST<Integer> tst = new TST<Integer>();
-        tst.put("she", 1);
-        tst.put("shore", 2);
-        tst.put("sheet", 3);
-
         Iterator<String> it = tst.keysThatMatch("sh...").iterator();
-        assert(it.hasNext());
-        it.next();
-        assert(it.hasNext());
-        it.next();
-        assert(it.hasNext() == false);
+        assertFalse(it.hasNext());
     }
 }
--- vvs_assignment_1/src/test/java/sut/coverage/line/TestTSTLongestPrefixOf.java
+++ vvs_assignment_1/src/test/java/sut/coverage/line/TestTSTLongestPrefixOf.java
@@ -9,6 +9,7 @@ public class TestTSTLongestPrefixOf {
     
     @Test
     public void testWithNullQuery() {
+        // linhas 136, 137
         assertThrows(IllegalArgumentException.class, () -> {
             new TST<Integer>().longestPrefixOf(null);
         });
@@ -16,23 +17,19 @@ public class TestTSTLongestPrefixOf {
 
     @Test
     public void testWithEmptyQuery() {
+        // linhas 136, 138, 139
         TST<Integer> tst = new TST<Integer>();
         assertNull(tst.longestPrefixOf(""));
     }
 
+    //para fazer line coverage
+    //basta fazer este teste que passa pelo middle
     @Test
-    public void testWithoutContaining() {
-        TST<Integer> tst = new TST<Integer>();
-        tst.put("she", 1);
-        assertEquals("", tst.longestPrefixOf("shells"));
-    }
-    
-    @Test
-    public void testNormalCase() {
-        TST<Integer> tst = new TST<Integer>();
-        tst.put("shore", 1);
-        tst.put("sheet", 2);
-        tst.put("she", 3);
-        assertEquals("she", tst.longestPrefixOf("shells"));
+    public void testMiddleMatchThenExit() {
+        // linhas 136, 138, 140, 141, 142, 143, 144, 
+        // 145, 146, 148, 149, 150, 151, 154
+        TST<Integer> tst = new TST<>();
+        tst.put("c", 1);
+        assertEquals("c", tst.longestPrefixOf("c"));
     }
 }
