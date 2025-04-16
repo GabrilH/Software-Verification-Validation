9c9791352b31726d9747d4f7181497407224c283
Gabriel Henriques <fc58182@alunos.fc.ul.pt>
2025-04-16 19:35:43 +0100

    completed ACUP

--- vvs_assignment_1/src/test/java/sut/coverage/allcouplingusepaths/TestTSTPut.java
+++ vvs_assignment_1/src/test/java/sut/coverage/allcouplingusepaths/TestTSTPut.java
@@ -9,16 +9,16 @@ public class TestTSTPut {
     
     
     @Test
-    public void testKeyAllCUC() {
-        // 1. Test (key: 1 -> i)
+    public void testKeyAllCUP() {
+        // Test (key: 1 -> i)
         TST<Integer> tst = new TST<>();
         tst.put("ab", 5);
         assertEquals(Integer.valueOf(5) , tst.get("ab"));
     }
 
     @Test
-    public void testValAllCUC() {
-        // 2. Test (val: 1 -> vii)
+    public void testValAllCUP() {
+        // Test (val: 1 -> vii)
         TST<Integer> tst = new TST<>();
         tst.put("cc", 10);
         tst.put("dd", 5);
@@ -27,30 +27,72 @@ public class TestTSTPut {
     }
 
     @Test
-    public void testX1AllCUC() {
-        // 3. Test (x: 3 -> ii)
+    public void testX1AllCUP() {
+        // Test (x: 3 -> ii)
         TST<Integer> tst = new TST<>();
         tst.put("aa", 10);
         assertEquals(Integer.valueOf(10), tst.get("aa"));
     }
 
     @Test
-    public void testX2AllCUC() {
-        // 3. Test (x: v -> 11)
+    public void testX2AllCUP() {
+        // Test (x: 1 -> ii)
         TST<Integer> tst = new TST<>();
-        tst.put("cc", 10);
-        tst.put("bb", 5);
-        tst.put("aa", 5);	
-        assertEquals(Integer.valueOf(5), tst.get("aa"));
+        tst.put("aa", 10);
+        assertEquals(Integer.valueOf(10), tst.get("aa"));
+    }
 
+    @Test
+    public void testD1AllCUP() {
+        // Test (d: 1 -> i)
+        TST<Integer> tst = new TST<>();
+        tst.put("aa", 10);
+        tst.put("ab", 5);
+        assertEquals(Integer.valueOf(5), tst.get("ab"));
     }
         
     @Test
-    public void testDAllCUC() {
-        // 4. Test (d: 9 -> i)
+    public void testD2AllCUP() {
+        // Test (d: 9 -> i)
         TST<Integer> tst = new TST<>();
         tst.put("ab", 5);
         tst.put("aa", 10);
         assertEquals(Integer.valueOf(10), tst.get("aa"));
+    }
+
+    @Test
+    public void testX_1AllCUP() {
+        // Test (x': v -> 11)
+        TST<Integer> tst = new TST<>();
+        tst.put("cc", 10);
+        tst.put("bb", 6);
+        tst.put("aa", 5);	
+        assertEquals(Integer.valueOf(5), tst.get("aa"));
+    }
+
+    @Test
+    public void testX_2AllCUP() {
+        // Test (x': vii -> 11)
+        TST<Integer> tst = new TST<>();
+        tst.put("cc", 10);
+        tst.put("aa", 6);
+        tst.put("bb", 5);
+        assertEquals(Integer.valueOf(5), tst.get("bb"));
+    }
+
+    @Test
+    public void testX_3AllCUP() {
+        // Test (x': ix -> 11)
+        TST<Integer> tst = new TST<>();
+        tst.put("cc", 10);
+        tst.put("ccb", 6);
+        assertEquals(Integer.valueOf(6), tst.get("cb"));
+    }
+
+    @Test
+    public void testX_4AllCUP() {
+        // Test (x': x -> 11)
+        TST<Integer> tst = new TST<>();
+        tst.put("cc", 10);
     }
 }
